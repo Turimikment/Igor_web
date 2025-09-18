12c3178a2ec5f2cd5a200e8abfccf199dacdd3b5
Turimikment <mentorturimik@gmail.com>
2025-09-18 15:50:29 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,21 @@
- 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+
+db = SQLAlchemy()
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('app.config.Config')
+    
+    db.init_app(app)
+    jwt.init_app(app)
+    
+    from app.auth import auth_bp
+    from app.routes import main_bp
+    
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(main_bp)
+    
+    return app
