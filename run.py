c47b6057ebd5236a0f2300487191ef9fdf7e3a42
Turimikment <mentorturimik@gmail.com>
2025-09-18 16:39:50 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
-from app import create_app, db
+from app import create_app
 
 app = create_app()
-
-# Всегда создаем таблицы при запуске приложения
-with app.app_context():
-    db.create_all()
-    print("Database tables created")
 
 if __name__ == '__main__':
     app.run(debug=False)
