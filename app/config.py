67a5dac4baaf3fd0449cabbe5ade87b4d4cddd94
Turimikment <mentorturimik@gmail.com>
2025-09-18 15:42:32 +0300

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://username:password@localhost/dbname')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt-secret-key') 
