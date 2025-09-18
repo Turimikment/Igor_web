02a9b1a7ba1bee7584c497465d2446533cf9a90d
Turimikment <mentorturimik@gmail.com>
2025-09-18 16:00:49 +0300

    Update run.py

--- run.py
+++ run.py
@@ -5,4 +5,7 @@ app = create_app()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    
+    # Для продакшена используйте gunicorn
+    if __name__ == '__main__':
+        app.run(debug=False)
